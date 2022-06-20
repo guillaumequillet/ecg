02fb8892a4482ee2823e78841b02bf4854afa75f
guillaumequillet <bgachats@yahoo.fr>
2022-06-20 20:52:13 +0200

    added multiple states

--- init.rb
+++ init.rb
@@ -11,14 +11,18 @@ class Window < Gosu::Window
     def button_down(id)
         super
         close! if id == Gosu::KB_ESCAPE
+        @current_state += 1 if id == Gosu::KB_RIGHT
+        @current_state -= 1 if id == Gosu::KB_LEFT
+        @current_state = @current_state.clamp(0, @states.size - 1)
     end
 
     def update
-
+        @states ||= [:fine, :warning, :danger]
+        @current_state ||= 0
     end
 
     def draw
-        @ecg.draw(:fine, 10, 10, 48, 24)
+        @ecg.draw(@states[@current_state], 10, 10, 48, 24)
     end
 end
 
